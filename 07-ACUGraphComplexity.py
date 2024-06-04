6009e05cf87f23782b84b7defc16888c374c0313
izaak-coleman <izaak.coleman1@gmail.com>
2024-06-04 09:51:30 -0400

    Color change

--- 07-ACUGraphComplexity.py
+++ 07-ACUGraphComplexity.py
@@ -5,9 +5,18 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 DATA = './data/ACUGraphComplexity'
+
+LABELS = {
+    '0.005': 'Copangraph (0.005)',
+    '0.01': 'Copangraph (0.01)',
+    '0.02': 'Copangraph (0.02)',
+    '0.05': 'Copangraph (0.05)',
+    '0.1': 'Copangraph (0.1)',
+    '0.2': 'Megahit graph',
+    }
 if __name__ == '__main__':
    
-    #df = pd.read_csv(os.path.join(DATA, 'complexity_results.csv'), index_col=0)
+    df = pd.read_csv(os.path.join(DATA, 'complexity_results.csv'), index_col=0)
    
     ## melt copangraph ms to single column
     #non_copan = df.loc[df.assembler != 'copangraph', :]
@@ -25,13 +34,25 @@ if __name__ == '__main__':
     #    plt.tight_layout()
     #    plt.savefig(os.path.join(DATA, f'{m}_complexity.pdf'), dpi=1400, bbox_inches='tight')
     #    
-    df = pd.read_csv(os.path.join(DATA, 'complexity_results_dt.csv'), index_col=0)
+    mh = df.loc[df.assembler == 'megahit', :]
+    mh.assembler = mh.assembler.apply(lambda x: f'{x}_graph')
+    df_dt = pd.read_csv(os.path.join(DATA, 'complexity_results_dt.csv'), index_col=0)
+    df_dt = pd.concat([df_dt, mh], ignore_index=True)
+    df_dt = df_dt.fillna(0.2)
+    df_dt['labels'] = df_dt.index.to_series().apply(
+        lambda i: df_dt.loc[i, 'assembler'] + ' ' + str(df_dt.loc[i, 'dt'])
+    )
+    
+    print(df_dt)
     # plot
     for m in ['nodes', 'edges', 'N50', 'N90']:
         plt.clf()
-        m_df = df.loc[df.metric == m, :]
-        sns.lineplot(x=m_df.n_samples, y=m_df.value, hue=m_df.dt)
+        m_df = df_dt.loc[df_dt.metric == m, :]
+        sns.lineplot(x=m_df.n_samples, y=m_df.value, hue=m_df.dt, palette='flare')
         plt.tight_layout()
+        legend = plt.gca().get_legend()
+        handles, labels = legend.legendHandles, legend.get_texts()
+        plt.legend(handles=handles, labels=[LABELS[e.get_text()] for e in labels])
         plt.savefig(os.path.join(DATA, f'{m}_complexity_dt.pdf'), dpi=1400, bbox_inches='tight')
     
    
